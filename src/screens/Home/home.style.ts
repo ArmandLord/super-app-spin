590f6841e84479248fbe7cbde49aa9c03379e306
Esteban-Reyes-Anguiano <139920025+Esteban-Reyes-Anguiano@users.noreply.github.com>
2023-08-07 15:12:32 -0600

    Screen Home completed

--- src/screens/Home/home.style.ts
+++ src/screens/Home/home.style.ts
@@ -2,6 +2,7 @@ import {StyleSheet} from 'react-native';
 
 export const styles = StyleSheet.create({
   container: {
+    backgroundColor: 'white',
     width: '100%',
     height: '100%',
     display: 'flex',
