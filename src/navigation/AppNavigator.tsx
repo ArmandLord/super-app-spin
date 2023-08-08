cabe34e23636a17417589ee025082ee53d0cdafb
Alberto Hermosillo <alberto.hermosillo@digitalfemsa.com>
2023-08-08 18:16:39 -0500

    home d2

--- src/navigation/AppNavigator.tsx
+++ src/navigation/AppNavigator.tsx
@@ -1,6 +1,6 @@
 import { createStackNavigator } from "@react-navigation/stack";
 import Transactions from "../screens/Transactions";
-import TransactionsDetails from "../screens/TransactionsDetails";
+import TransactionsDetails from "../screens/TransactionDetails";
 import TabsNavigator from "./TabsNavigator";
 import { NavigationContainer } from "@react-navigation/native";
 
@@ -10,17 +10,21 @@ const AppNavigator = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-          <Stack.Screen 
-          name="Tabs" 
-          component={TabsNavigator} 
+        <Stack.Screen
+          name="Tabs"
+          component={TabsNavigator}
           options={{
             headerShown: false,
-          }}/>
-          <Stack.Screen name="Transactions" component={Transactions} />
-          <Stack.Screen name="TransactionsDetails" component={TransactionsDetails} />
+          }}
+        />
+        <Stack.Screen name="Transactions" component={Transactions} />
+        <Stack.Screen
+          name="TransactionsDetails"
+          component={TransactionsDetails}
+        />
       </Stack.Navigator>
     </NavigationContainer>
-  )
+  );
 }
 
 export default AppNavigator;
