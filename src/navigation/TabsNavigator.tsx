fcfdb6e67dd74402cb34026f8349b522c492eef7
Alberto Hermosillo <alberto.hermosillo@digitalfemsa.com>
2023-08-09 13:41:20 -0500

    feature home d3

--- src/navigation/TabsNavigator.tsx
+++ src/navigation/TabsNavigator.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { Image } from 'react-native';
-
+import { Image, Text, StyleSheet } from 'react-native';
 
 import HomeScreen from '../screens/HomeScreen';
 import AccountScreen from '../screens/AccountScreen';
@@ -9,7 +8,6 @@ import BenefitScreen from '../screens/BenefitScreen';
 import WalletScreen from '../screens/WalletScreen';
 
 const Tab = createBottomTabNavigator();
-
 
 const TabsNavigator = () => {
   return (
@@ -20,53 +18,95 @@ const TabsNavigator = () => {
       <Tab.Screen
         name="Home"
         component={HomeScreen}
-        options={{
-          tabBarIcon: ({color, size}) => (
+        options={({ route }) => ({
+          tabBarIcon: ({ color, size, focused }) => (
             <Image
-              source={require('../assets/icon_home.png')}
-              style={{width: size, height: size, tintColor: '#69698B'}}
+              source={
+                focused
+                  ? require('../assets/icon_home_press.png')
+                  : require('../assets/icon_home.png')
+              }
+              style={{ width: size, height: size, tintColor: focused ? '#05053D' : '#69698B' }}
             />
           ),
-        }}
+          tabBarLabel: ({ focused }) => (
+            <Text style={[styles.tabLabel, { color: focused ? '#05053D' : '#69698B', fontWeight: focused ? 'bold' : 'normal' }]}>
+              Home
+            </Text>
+          ),
+        })}
       />
       <Tab.Screen
         name="Beneficios"
         component={BenefitScreen}
-        options={{
-          tabBarIcon: ({color, size}) => (
+        options={({ route }) => ({
+          tabBarIcon: ({ color, size, focused }) => (
             <Image
-              source={require('../assets/icon_benefit.png')}
-              style={{width: size, height: size, tintColor: '#05053D'}}
+              source={
+                focused
+                  ? require('../assets/icon_benefit_press.png')
+                  : require('../assets/icon_benefit.png')
+              }
+              style={{ width: size, height: size, tintColor: focused ? '#05053D' : '#69698B' }}
             />
           ),
-        }}
+          tabBarLabel: ({ focused }) => (
+            <Text style={[styles.tabLabel, { color: focused ? '#05053D' : '#69698B', fontWeight: focused ? 'bold' : 'normal' }]}>
+              Beneficios
+            </Text>
+          ),
+        })}
       />
       <Tab.Screen
         name="Cartera"
         component={WalletScreen}
-        options={{
-          tabBarIcon: ({color, size}) => (
+        options={({ route }) => ({
+          tabBarIcon: ({ color, size, focused }) => (
             <Image
-              source={require('../assets/icon_wallet.png')}
-              style={{width: size, height: size, tintColor: '#05053D'}}
+              source={
+                focused
+                  ? require('../assets/icon_wallet.png')
+                  : require('../assets/icon_wallet.png')
+              }
+              style={{ width: size, height: size, tintColor: focused ? '#05053D' : '#69698B' }}
             />
           ),
-        }}
+          tabBarLabel: ({ focused }) => (
+            <Text style={[styles.tabLabel, { color: focused ? '#05053D' : '#69698B', fontWeight: focused ? 'bold' : 'normal' }]}>
+              Cartera
+            </Text>
+          ),
+        })}
       />
       <Tab.Screen
         name="Cuenta"
         component={AccountScreen}
-        options={{
-          tabBarIcon: ({color, size}) => (
+        options={({ route }) => ({
+          tabBarIcon: ({ color, size, focused }) => (
             <Image
-              source={require('../assets/icon_account.png')}
-              style={{width: size, height: size, tintColor: '#69698B'}}
+              source={
+                focused
+                  ? require('../assets/icon_account_press.png')
+                  : require('../assets/icon_account.png')
+              }
+              style={{ width: size, height: size, tintColor: focused ? '#05053D' : '#69698B' }}
             />
           ),
-        }}
+          tabBarLabel: ({ focused }) => (
+            <Text style={[styles.tabLabel, { color: focused ? '#05053D' : '#69698B', fontWeight: focused ? 'bold' : 'normal' }]}>
+              Cuenta
+            </Text>
+          ),
+        })}
       />
     </Tab.Navigator>
   );
 };
+
+const styles = StyleSheet.create({
+  tabLabel: {
+    fontSize: 12,
+  },
+});
 
 export default TabsNavigator;
