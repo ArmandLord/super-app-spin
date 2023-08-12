900e624d1540617799ad53c90587876afbcc54e1
geyser-digitalfemsa <139982172+geyser-digitalfemsa@users.noreply.github.com>
2023-08-11 21:07:04 -0600

    corrección del tipado de la navegación

--- src/navigation/NavListBase.ts
+++ src/navigation/NavListBase.ts
@@ -1,5 +1,5 @@
-import {StackNavigationProp} from '@react-navigation/stack';
-import {RouteProp} from '@react-navigation/native';
+import { StackNavigationProp } from '@react-navigation/stack';
+import { RouteProp } from '@react-navigation/native';
 
 export type NavListBase = {
   TransactionsScreen: undefined;
@@ -10,35 +10,13 @@ export type NavListBase = {
   TicketScreen: undefined;
 };
 
-// Define el tipo de navegación para las pantallas del stack
-export type AppStackNavigationProp = StackNavigationProp<
-  NavListBase,
-  'TransactionsScreen',
-  'PointsScreen'
->;
-
-// Define el tipo de ruta para las pantallas del stack
-export type AppStackRouteProp = RouteProp<NavListBase, 
-|'TransactionsScreen'
-|'PointsScreen'
-|'Tabs'
-|'BalanceScreen'
-|'TransactionsDetailsScreen'
-|'TicketScreen'>;
-
-export type DetailsScreenProps = {
-  navigation: StackNavigationProp<NavListBase, 
-  |'TransactionsScreen'
-  |'PointsScreen'
-  |'Tabs'
-  |'BalanceScreen'
-  |'TransactionsDetailsScreen'
-  |'TicketScreen'>;
-  route: RouteProp<NavListBase, 
-  |'TransactionsScreen'
-  |'PointsScreen'
-  |'Tabs'
-  |'BalanceScreen'
-  |'TransactionsDetailsScreen'
-  |'TicketScreen'>;
+// Define los tipos de navegación y ruta para las pantallas del stack
+type AppStackProps = {
+  navigation: StackNavigationProp<NavListBase>;
+  route: RouteProp<NavListBase>;
 };
+
+export type AppStackNavigationProp = AppStackProps['navigation'];
+export type AppStackRouteProp = AppStackProps['route'];
+
+export type DetailsScreenProps = AppStackProps;
